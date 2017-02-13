scikit-build/github-release

commit b4c9592c6cedb880e31e121cda8962fdbf596753

style: Refactor gh_asset_erase to nicely report status messages
For example, without verbose:

```
$ githubrelease asset jcfr/sandbox erase nightly "*cp35*" --keep-pattern "*win*" --dry-run
deleting 'nightly' release asset(s) (matched: 4, matched-and-keep: 2, not-matched: 12):
  deleting sandbox-2.0.0.dev20170105-cp35-cp35m-macosx_10_11_x86_64.whl
  deleting sandbox-2.0.0.dev20170105-cp35-cp35m-manylinux1_x86_64.whl
```

and with verbose:

```
$ githubrelease asset jcfr/sandbox erase nightly "*cp35*" --keep-pattern "*win*" --dry-run --verbose
deleting 'nightly' release asset(s) (matched: 4, matched-but-keep: 2, not-matched: 12):
  deleting sandbox-2.0.0.dev20170105-cp35-cp35m-macosx_10_11_x86_64.whl
  deleting sandbox-2.0.0.dev20170105-cp35-cp35m-manylinux1_x86_64.whl
  skipping sandbox-2.0.0.dev20170105-cp35-cp35m-win32.whl (match keep_pattern '*win*')
  skipping sandbox-2.0.0.dev20170105-cp35-cp35m-win_amd64.whl (match keep_pattern '*win*')

  assets NOT matching selection pattern [*cp35*]:
    sandbox-2.0.0.dev20170105-cp34-cp34m-win_amd64.whl
    sandbox-2.0.0.dev20170105-cp36-cp36m-win32.whl
    sandbox-2.0.0.dev20170105-cp27-cp27m-manylinux1_x86_64.whl
    sandbox-2.0.0.dev20170105-cp36-cp36m-win_amd64.whl
    sandbox-2.0.0.dev20170105-cp34-cp34m-manylinux1_x86_64.whl
    sandbox-2.0.0.dev20170105-cp34-cp34m-macosx_10_11_x86_64.whl
    sandbox-2.0.0.dev20170105-cp27-cp27m-win32.whl
    sandbox-2.0.0.dev20170105-cp34-cp34m-win32.whl
    sandbox-2.0.0.dev20170105-cp27-cp27m-macosx_10_11_x86_64.whl
    sandbox-2.0.0.dev20170105-cp36-cp36m-manylinux1_x86_64.whl
    sandbox-2.0.0.dev20170105-cp36-cp36m-macosx_10_11_x86_64.whl
    sandbox-2.0.0.dev20170105-cp27-cp27m-win_amd64.whl
```

github_release.py
@@ -415,16 +415,36 @@ gh_asset_upload.description = {
 def gh_asset_erase(repo_name, tag_name, pattern,
                    keep_pattern=None, dry_run=False, verbose=False):
     release = get_release_info(repo_name, tag_name)
+    # List of assets
+    excluded_assets = {}
+    matched_assets = []
+    matched_assets_to_keep = {}
     for asset in release['assets']:
         if not fnmatch.fnmatch(asset['name'], pattern):
+            skip_reason = "do NOT match pattern '%s'" % pattern
+            excluded_assets[asset['name']] = skip_reason
             continue
+        matched_assets.append(asset)
         if keep_pattern is not None:
             if fnmatch.fnmatch(asset['name'], keep_pattern):
-                if verbose:
-                    print('skipping asset {0}: do not match {1}'.format(
-                        asset['name'], keep_pattern))
+                skip_reason = "match keep_pattern '%s'" % keep_pattern
+                matched_assets_to_keep[asset['name']] = skip_reason
                 continue
-        print('release {0}: deleting {1}'.format(tag_name, asset['name']))
+    # Summary
+    summary = "matched: %s, matched-but-keep: %s, not-matched: %s" % (
+        len(matched_assets),
+        len(matched_assets_to_keep),
+        len(excluded_assets)
+    )
+    print("deleting '%s' release asset(s) (%s):" % (tag_name, summary))
+    # Perform deletion
+    for asset in matched_assets:
+        if asset['name'] in matched_assets_to_keep:
+            if verbose:
+                skip_reason = matched_assets_to_keep[asset['name']]
+                print("  skipping %s (%s)" % (asset['name'], skip_reason))
+            continue
+        print("  deleting %s" % asset['name'])
         if dry_run:
             continue
         url = (
@@ -432,8 +452,14 @@ def gh_asset_erase(repo_name, tag_name, pattern,
             + '/repos/{0}/releases/assets/{1}'.format(repo_name, asset['id'])
         )
         response = _request('DELETE', url)
-
         response.raise_for_status()
+    print("")
+    if verbose:
+        indent = "  "
+        print(indent + "assets NOT matching selection pattern [%s]:" % pattern)
+        for asset_name in excluded_assets:
+            print(indent + "  " + asset_name)
+        print("")
 
 
 gh_asset_erase.description = {

commit 230c30b638bef63be1e45376ee9be284bb466145

style: gh_asset_erase: Ensure "nothing to delete" is displayed when needed
This commit ensures "nothing to delete" is displayed only when no assets are
deleted.

Problem was originally introduced in b4c9592 (style: Refactor gh_asset_erase
to nicely report
status messages)

github_release.py
@@ -470,6 +470,8 @@ def gh_asset_erase(repo_name, tag_name, pattern,
         )
         response = _request('DELETE', url)
         response.raise_for_status()
+    if len(matched_assets) == 0:
+        print("  nothing to delete")
     print("")
     if verbose:
         indent = "  "