af438b13cca0a5c4601623e46ac35e393d9cae91
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-02-17 16:48:03 -0500

    setup.py: Improve readbility

--- setup.py
+++ setup.py
@@ -12,20 +12,24 @@ setup_requires = ['setuptools-version-command']
 
 setup(
     name='githubrelease',
-    version_command='git describe',
+
+    url='https://github.com/j0057/github-release',
+
     author='Joost Molenaar, Jean-Christophe Fillion-Robin',
     author_email='j.j.molenaar@gmail.com, jchris.fillionr@kitware.com',
-    url='https://github.com/j0057/github-release',
+
+    version_command='git describe',
+
     py_modules=['github_release'],
-    install_requires=requirements,
-    tests_require=dev_requirements,
-    setup_requires=setup_requires,
     entry_points={
         'console_scripts': [
             'githubrelease = github_release:main',
             'github-release = github_release:gh_release',
             'github-asset = github_release:gh_asset'
         ]},
+
+    license="Apache",
+
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -37,5 +41,8 @@ setup(
         'Topic :: Software Development :: Version Control',
         'Topic :: System :: Software Distribution'
     ],
-    license="Apache",
+
+    install_requires=requirements,
+    tests_require=dev_requirements,
+    setup_requires=setup_requires
 )
