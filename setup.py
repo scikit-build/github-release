c7b63523a0fab0936d5adfdb58b19bfd300ef141
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-02-18 01:48:45 -0500

    Add infrastructure to run pytest using tox and TravisCI

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import setup
 
 with open('README.md', 'r') as fp:
@@ -12,6 +14,13 @@ with open('requirements-dev.txt', 'r') as fp:
     dev_requirements = list(filter(bool, (line.strip() for line in fp)))
 
 setup_requires = ['setuptools-version-command']
+
+# Require pytest-runner only when running tests
+pytest_runner = (['pytest-runner>=2.0,<3dev']
+                 if any(arg in sys.argv for arg in ('pytest', 'test'))
+                 else [])
+
+setup_requires.extend(pytest_runner)
 
 setup(
     name='githubrelease',
@@ -28,6 +37,7 @@ setup(
     version_command='git describe',
 
     py_modules=['github_release'],
+
     entry_points={
         'console_scripts': [
             'githubrelease = github_release:main',
